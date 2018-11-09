6631cfcc3d02ac4e773d9283582b19e1a51eb78d
Sasha Gomel <fedordrv8dr@mail.ru>
2018-11-09 18:34:16 +0300

    moved temlates

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,8 +14,22 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
+        return $this->render('@App/default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/feedback", name="feedback")
+     */
+    public function feedbackAction(Request $request){
+        return $this->render("@App/default/feedback.html.twig", [
+            "asd" => "this is first variable",
+            "myarr" => [
+                "el1" => "first arr",
+                "el2" => 127,
+                "el3" => "element 3"
+            ]
         ]);
     }
 }
